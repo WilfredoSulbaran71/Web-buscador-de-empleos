294d9cdfb0fb4890aab3fafbca0082b2646a94c6
Wilfredo Sulbaran <wilbaran@Mac-mini-de-Wilfredo.local>
2025-10-15 21:28:27 -0400

    Actualizacion en logica de botones  archivos js

--- 00-htm-css/logicas.js
+++ 00-htm-css/logicas.js
@@ -1,3 +1,6 @@
+
+// Lógica para cambiar el texto del botón al hacer clic
+/*
 const boton = document.querySelectorAll('.btn-aplicar')
 
 
@@ -10,3 +13,17 @@ boton.forEach(boton => {
     })
 })
 
+*/
+
+// Lógica moderna para cambiar el texto del botón al hacer clic
+
+const jobsListingSection = document.querySelector('.jobs-listings')
+jobsListingSection.addEventListener('click', function(event) {
+    const element = event.target
+    if (element.classList.contains('btn-aplicar')) {
+        element.textContent = 'Aplicado'
+        element.classList.add('selec')
+        element.disabled = true
+    }
+})
+// Nota: Asegúrate de que el archivo styles.css tenga la clase .seleccionado definida para cambiar el estilo del botón cuando se aplica.
